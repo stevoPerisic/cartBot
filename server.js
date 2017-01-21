64201fb6642f087bb24d7120441ed39e70779815
Stevo Perisic <stevo.perisic@maestralsolutions.com>
2017-01-21 09:34:47 -0500

    added some basic routes and more stuff, it's been a while....

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var app = express();
 mongoose.connect(process.env.MONGODB);
 mongoose.connection.on('error', function() {
   console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
-  process.exit(1);
+  // process.exit(1);
 });
 app.set('port', process.env.PORT || 3000);
 app.use(compression());
